c3f029cde18334ac86c8f1c1cb8108eeb7b9d8ec
bbtechpro <billy.barber@gmail.com>
2026-05-20 20:33:56 -0400

    Got rid of deprecated parameters

--- config/connection.js
+++ config/connection.js
@@ -5,10 +5,7 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
+mongoose.connect(process.env.MONGO_URI)
 .then(() => console.log('Connected to MongoDB'))
 .catch((error) => console.error('Error connecting to MongoDB:', error));
 
